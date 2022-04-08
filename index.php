c90e7c405362436fca989d640805eda0fd76d6ad
vusal11010 <vusal11010@gmail.com>
2022-04-08 14:32:36 +0400

    Renamed PHP file and connected database

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
+<?php
+
+try {
+    $db = new PDO("mysql:host=localhost;dbname=in_class_work", "root", "");
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
